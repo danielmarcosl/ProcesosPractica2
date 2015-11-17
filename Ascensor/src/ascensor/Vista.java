c30eedb280005af691447292123cbca14bb3241f
Daniel Marcos Lorrio <danielmarcos.to@gmail.com>
2015-11-17 15:47:51 +0100

    Create simple windows form

--- Ascensor/src/ascensor/Vista.java
+++ Ascensor/src/ascensor/Vista.java
@@ -7,4 +7,19 @@ package ascensor;
  */
 public class Vista {
     
+    private JFrame ventana = new JFrame();
+    private TextArea texto_ventana = new TextArea();
+
+    /**
+     * Creacion de ventana de Windows
+     * 
+     * @param nombre Nombre de la ventana
+     */
+    Vista(String nombre) {
+        ventana.setName(nombre);
+        ventana.setTitle(nombre);
+        ventana.setSize(400, 400);
+        ventana.setVisible(true);
+        ventana.add(texto_ventana);
+    }
 }
